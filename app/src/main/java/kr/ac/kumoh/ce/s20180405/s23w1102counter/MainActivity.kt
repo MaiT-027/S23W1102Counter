b4ef36c626c2c32370de21d82e6bdf9f7381a86b
MaiT <maitmus@gmail.com>
2023-11-15 16:24:34 +0900

    .

--- app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w1102counter/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w1102counter/MainActivity.kt
@@ -17,20 +17,24 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20180405.s23w1102counter.ui.theme.S23W1102CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val vm = ViewModelProvider(this)[CounterViewModel::class.java]
         super.onCreate(savedInstanceState)
         setContent {
-            //MyApp(content = { Greeting("test") })
             MyApp {
                 Column(
                     modifier = Modifier
@@ -38,7 +42,7 @@ class MainActivity : ComponentActivity() {
                         .padding(8.dp),
                     verticalArrangement = Arrangement.Center,
                 ) {
-                    Counter()
+                    Counter(vm)
                 }
             }
         }
@@ -62,7 +66,7 @@ fun MyApp(content: @Composable () -> Unit) {
 fun Clicker() {
 //    var txtString = "눌러주세요"
 //    var txtString by remember { mutableStateOf("눌러주세요") }
-    val (txtString, setTxtString) = remember { mutableStateOf("눌러주세요") }
+    val (txtString, setTxtString) = rememberSaveable { mutableStateOf("눌러주세요") }
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(8.dp),
@@ -83,19 +87,20 @@ fun Clicker() {
 }
 
 @Composable
-fun Counter() {
-    val (counterValue, setCounterValue) = remember {mutableStateOf(0)}
+fun Counter(viewModel: CounterViewModel) {
+    val count by viewModel.count.observeAsState(0)
+    val (counterValue, setCounterValue) = rememberSaveable {mutableStateOf(0)}
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = counterValue.toString(), fontSize = 70.sp,)
-        Button(onClick = { setCounterValue(counterValue + 1) }, modifier = Modifier.fillMaxWidth()) {
+        Text(text = count.toString(), fontSize = 70.sp,)
+        Button(onClick = { viewModel.onAdd() }, modifier = Modifier.fillMaxWidth()) {
             Text(text = "증가")
         }
         Row {
-            Button(onClick = { if (counterValue > 0) setCounterValue(counterValue - 1) }, modifier = Modifier.weight(1f, true)) {
+            Button(onClick = { viewModel.onSub() }, modifier = Modifier.weight(1f, true)) {
                 Text(text = "감소")
             }
             Spacer(modifier = Modifier.width(8.dp))
